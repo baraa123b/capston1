233b1849b149a3cc9bdd287dee902f0a1eb4460e
baraa123b <baraaadnan1@hotmail.com>
2024-11-22 13:39:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from PIL import Image
 #st.title("Display Local Image")
 
 # Display the image from your desktop
-image_path = "C:\\Users\\best\\Desktop\\SADA.png"  # Replace with your image path
+image_path = "C:\Users\best\Desktop\SADA.png"  # Replace with your image path
 st.image(image_path, caption="This is SADA!", use_column_width=True)
 
 
