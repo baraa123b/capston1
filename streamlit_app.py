71972a72311e48d803a6f89f3028a3a639cc67c0
baraa123b <baraaadnan1@hotmail.com>
2024-11-22 13:36:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
-
+from PIL import Image
 #st.title('SADA APP')
 
 #st.write('Hello world!')
+
+# Title
+#st.title("Display Local Image")
+
+# Display the image from your desktop
+image_path = "C:\\Users\\best\\Desktop\\SADA.png"  # Replace with your image path
+st.image(image_path, caption="This is SADA!", use_column_width=True)
+
+
