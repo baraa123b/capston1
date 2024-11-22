545cab2d640d5d0e5570279c75687b07eb9faa74
baraa123b <baraaadnan1@hotmail.com>
2024-11-22 13:57:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,9 @@ from PIL import Image
 
 import streamlit as st
 
-# Correct file path as a raw string
-image_path = "SADA.png"
+# Relative path to the image inside the 'images' folder
+image_path = "images/SADA.png"  # Use the path relative to your script
+
+# Display the image
 st.image(image_path, caption="This is SADA!", use_column_width=True)
 
